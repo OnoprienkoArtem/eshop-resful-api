b9b90b42b02475daf33dc2821e9ea60de001b8b0
OnoprienkoArtem <o.artem.e@gmail.com>
2022-02-02 23:50:24 +0200

    create env variable 1

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
 app.listen(3000, () => {
+    console.log(api);
     console.log('server is running http://localhost:3000');
 });
