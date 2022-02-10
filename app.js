b868f5877482d9374ea3f06090f1f4f4ee8305e6
OnoprienkoArtem <o.artem.e@gmail.com>
2022-02-10 23:38:42 +0200

    create derictory routers

--- app.js
+++ app.js
@@ -1,47 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const app = express();
 
+const productsRouter = require('./routers/products');
+
+const app = express();
 
 require('dotenv/config');
 
 const api = process.env.API_URL;
 
+// middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 
+// Routers
+app.use(`${api}/products`, productsRouter);
 
 
 
-const product = require('./models/product');
-app.get(`${api}/products`, async (req, res) => {
-    const productList = await Product.find();
-
-    if (!productList) {
-        res.status(500).json({success: false});
-    }
-    res.send(productList);
-});
-
-app.post(`${api}/products`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock,
-    });
-
-    product.save()
-        .then(createdProduct => {
-            res.status(201).json(createdProduct);
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err,
-                success: false
-            })
-        });
-});
 
 mongoose.connect(process.env.CONNECTION_STRING)
     .then(() => {
