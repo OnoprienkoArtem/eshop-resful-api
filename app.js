929981bb13bc447d068d333e0f671af192965835
OnoprienkoArtem <o.artem.e@gmail.com>
2022-02-12 23:56:11 +0200

    group all stuff in app.js

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
 const express = require('express');
+const app = express();
+
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
-const productsRouter = require('./routers/products');
-
-const app = express();
+require('dotenv/config');
 
 app.use(cors());
 app.options('*', cors());
-
-require('dotenv/config');
-
-const api = process.env.API_URL;
 
 // middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 
 // Routers
-app.use(`${api}/products`, productsRouter);
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
+const api = process.env.API_URL;
 
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
 
 mongoose.connect(process.env.CONNECTION_STRING)
-    .then(() => {
-        console.log('Database Connection is ready ...');
-    })
-    .catch(err => {
-        console.log(err);
-    });
+.then(() => console.log('Database Connection is ready ...'))
+.catch(err => console.log(err));
 
 app.listen(3000, () => {
     console.log(api);
