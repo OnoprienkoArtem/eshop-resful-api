40e85bcdeb70eab71540f24d45a3a91c56c5fede
OnoprienkoArtem <o.artem.e@gmail.com>
2022-02-17 23:56:03 +0200

    add delete category router

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,10 @@
 const {Category} = require('../models/category');
 const express = require('express');
 const e = require('express');
+const rules = require('nodemon/lib/rules');
 const router = express.Router();
 
-router.get(`/`, async (req, res) =>{
+router.get('/', async (req, res) =>{
     const categoryList = await Category.find();
 
     if(!categoryList) {
@@ -26,5 +27,18 @@ router.post('/', async (req, res) => {
 
     res.send(category);
 });
+
+router.delete('/:id', (req, res) => {
+    Category.findByIdAndRemove(req.params.id).then(category => {
+        if (category) {
+            return res.status(200).json({success: true, message: 'the category is deleted!'});
+        } else {
+            return res.status(404).json({success: false, message: 'category not found'});
+        }
+    }).catch(err => {
+        return res.status(400).json({success: false, error: err});
+    });
+});
+
 
 module.exports = router;
