19642c8680dcf6d68a3746907c4aa46f5e77c80c
OnoprienkoArtem <o.artem.e@gmail.com>
2022-02-21 23:32:27 +0200

    relate category to product

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 
 router.get('/', async (req, res) => {
-    const productList = await Product.find().select('name image -_id');
+    const productList = await Product.find().select('name image -_id').populate('category');
 
     if (!productList) {
         res.status(500).json({success: false});
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-    const product = await Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id).populate('category');
 
     if (!product) {
         res.status(500).json({success: false});
