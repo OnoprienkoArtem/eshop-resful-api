9d783a3ff2a574faabf4718c501e5250b2a8e9a7
OnoprienkoArtem <o.artem.e@gmail.com>
2022-05-18 23:26:34 +0300

    add filtering and getting products by categories

--- routes/products.js
+++ routes/products.js
@@ -6,8 +6,15 @@ const mongoose = require('mongoose');
 
 
 router.get('/', async (req, res) => {
-    const productList = await Product.find().populate('category');
-    // .select('name image -_id')
+    let filter = {};
+
+    if (req.query.categories) {
+        filter = {
+            category: req.query.categories.split(','),
+        };
+    }
+
+    const productList = await Product.find(filter).populate('category');
 
     if (!productList) {
         res.status(500).json({success: false});
