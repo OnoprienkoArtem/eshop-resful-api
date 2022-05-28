c8d3e7666827706adecfdcad5f4e81d45e6c2ad9
OnoprienkoArtem <o.artem.e@gmail.com>
2022-05-28 23:42:24 +0300

    add register route

--- routes/users.js
+++ routes/users.js
@@ -105,8 +105,29 @@ router.post('/login', async (req, res) => {
         res.status(200).send({user: user.email, token: token});
     } else {
         res.status(400).send('password is wrong!');
+    }    
+});
+
+router.post('/register', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    });
+    user = await user.save();
+
+    if (!user) {
+        return res.status(404).send('the user cannot be created!');
     }
-    
+
+    res.send(user);
 });
 
 module.exports = router;
